58c303ea8e287f60422e72455907008153458ad1
Joshua Padgett <jpadgett@jpadgett-a01-2.local>
2021-11-20 16:50:54 -0800

    Add video list with default search term

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const API_KEY = process.env.YOUTUBE_API_KEY;
+const YT_API_KEY = '';
 
-// Create a new component. This should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <div>Hi!</div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: YT_API_KEY, term: 'Jeeps'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
       <div>
+        <div>Hi!</div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-// Take component's generated HTML and put it in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
